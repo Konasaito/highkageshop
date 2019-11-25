b931f656962a9312f06d0f461930a62151270456
Mario Martins <mario.vnci@gmail.com>
2019-11-25 09:17:08 -0300

    deleted cd highkageshop &&
    line 5

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@ git add . &&
 git commit -m "Auto aiiii deploy" &&
 git push origin master &&
 ssh -t root@193.70.38.32 <<EOF
-cd highkageshop &&
 git pull origin master &&
 stack build &&
 lsof -i:443 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
