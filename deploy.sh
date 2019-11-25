03dec855c9e7ebb7ff3a05c8bd916ecfff8afbaf
Mario Martins <mario.vnci@gmail.com>
2019-11-25 02:50:35 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 git add . &&
 git commit -m "$1" &&
 git push origin master &&
-ssh root@51.91.126.117 <<EOF
-cd aulaadsn &&
+ssh root@193.70.38.32 <<EOF
+cd highkageshop &&
 git pull origin master &&
 stack build &&
 lsof -i:443 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
-nohup stack exec aulahaskell > /dev/null 
+nohup stack exec highkageshop > /dev/null 
 echo "deploy finished"
 EOF
