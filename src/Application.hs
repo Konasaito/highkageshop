1ffc8ad59343bd62ce2c9533d37a68d2141d9520
Mario Martins <mario.vnci@gmail.com>
2019-11-25 03:41:13 -0300

    update handlers

--- src/Application.hs
+++ src/Application.hs
@@ -41,9 +41,8 @@ import Handler.Common
 import Handler.Home
 import Handler.Usuario
 import Handler.Login
-import Handler.Ator
-import Handler.Serie
-import Handler.Atua
+import Handler.Cachimbos
+
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
