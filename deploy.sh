bfa8c5a12f2aa93b33f20a0b5268d321adeeb02c
Mario Martins <mario.vnci@gmail.com>
2019-11-27 21:24:09 -0300

    tirei o cd

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ git add . &&
 git commit -m "Auto deploy after update" &&
 git push origin master &&
 ssh root@193.70.38.32 <<EOF
-cd highkageshop &&
 git pull origin master &&
 stack build &&
 lsof -i:443 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
